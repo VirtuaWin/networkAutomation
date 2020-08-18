75dbf48a82e5addec360e1959cfbe80a98d263a3
VirtuaWin <anonemus73@gmail.com>
2020-08-18 23:04:21 +0100

    First version.

---
+++ forLoop.py
@@ -0,0 +1,11 @@
+for n in range(2, 11):
+    print("vlan" + str(n))
+
+# for loop will run while n is between 2 or 10 (11-1)
+# str(n) converts integer to string
+# we can use the type command in the Python Console to check the type of a variable
+# p.e. type(1) will return <class 'int'>
+# type("hello") will return <class 'str'>
+# in the same way, type(str(1)) will return <class 'str'>
+# we can also do n = 10 and then type(n)
+# notice the difference in the type command when n = 10 and n = '10'
