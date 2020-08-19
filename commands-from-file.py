4eea94dbe3d8cf53366878abba7a0049cc5c5c93
VirtuaWin <anonemus73@gmail.com>
2020-08-19 17:30:11 +0100

    Get commands from a file with the with function

---
+++ commands-from-file.py
@@ -0,0 +1,13 @@
+# Get configuration commands from a file
+with open('iou-l2-campus-cisco-design') as file:
+    lines = file.read().splitlines()
+print(lines)
+
+# In Python you need to give access to a file by opening it. You can do it by using the open() function.
+# Open returns a file object, which has methods and attributes for getting information about and manipulating
+# the opened file.
+# With the “With” statement, you get better syntax and exceptions handling.
+# In addition, it will automatically close the file. The with statement provides a way for ensuring that
+# a clean-up is always used.
+# Opening a file using with is as simple as: with open(filename) as file:
+
